1c1170a898f81e775305f83bc4d0a381d34fa64f
PeterLock <pclo2333@gmail.com>
2017-07-05 22:13:06 +0900

    Add files via upload

--- Collections/Collections/src/com/theatre/Main.java
+++ Collections/Collections/src/com/theatre/Main.java
@@ -1,33 +1,23 @@
 package com.theatre;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
-       Theatre theatre = new Theatre("Olympian", 8, 12 );
+	    Theatre theatre = new Theatre("Olympian", 8, 12);
         List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
         printList(seatCopy);
-
-        seatCopy.get(1).reserved();
-        if(theatre.reserveSeat("AO2")){
-            System.out.println("Please pay for AO2");
-        }else{
-            System.out.println("Seat already reserved");
-        }
-
 
 
     }
 
     public static void printList(List<Theatre.Seat> list){
-        for(Theatre.Seat seat : list){
-            System.out.print(" " + seat.getSeatNumber());
+        for(Theatre.Seat seat: list){
+            System.out.println(" " + seat.getSeatNumber());
         }
         System.out.println();
-        System.out.println("===========================================================================================================");
+        System.out.println("==================================");
     }
 }
