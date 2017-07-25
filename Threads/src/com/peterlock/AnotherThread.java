7ff6164e2898948d6b5884781d5104806e8563e0
PeterLock <pclo2333@gmail.com>
2017-07-25 12:00:23 +0900

    Add files via upload

---
+++ Threads/src/com/peterlock/AnotherThread.java
@@ -0,0 +1,18 @@
+package com.peterlock;
+
+
+import static com.peterlock.ThreadColor.ANSI_BLUE;
+
+public class AnotherThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+        try{
+            Thread.sleep(5000);
+        } catch(InterruptedException e){
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+            return;
+        }
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I'm awake");
+    }
+}
---
+++ Threads/src/com/peterlock/Main.java
@@ -0,0 +1,41 @@
+package com.peterlock;
+
+import static com.peterlock.ThreadColor.ANSI_GREEN;
+import static com.peterlock.ThreadColor.ANSI_PURPLE;
+import static com.peterlock.ThreadColor.ANSI_RED;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ANSI_PURPLE + "Hello from the main thread");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
+        anotherThread.start();
+
+        new Thread(){
+            public void run(){
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread");
+            }
+        }.start();
+
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run(){
+                System.out.println(ANSI_RED + "Hello from the anonymous class's implementation of run");
+                try{
+                    anotherThread.join();
+                    System.out.println("AnotherThread terminated, or timed out, so I'm running again");
+                }catch(InterruptedException e){
+                    System.out.println(ANSI_RED + "I could'nt wait after all. I was interrupted.");
+                }
+            }
+        });
+
+        myRunnableThread.start();
+//        anotherThread.interrupt();
+
+        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
+
+    }
+}
